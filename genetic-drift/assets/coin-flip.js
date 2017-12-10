5dcc3a281f46a783ab8f7d439b92d66e1b706128
Kowus <barnabas@elite-education.org>
2017-12-10 02:35:36 +0000

    interface optimization

--- genetic-drift/assets/coin-flip.js
+++ genetic-drift/assets/coin-flip.js
@@ -13,5 +13,18 @@ function toss_coins() {
     document.getElementById('heads').innerText = heads;
     document.getElementById('tails').innerText = tails;
 
-    console.log(heads, "heads ", tails, " tails");
+    // console.log(heads, "heads ", tails, " tails");
+
+    // Check heads appear 8 times/scaled up
+     return round_number(heads/rounds) === round_number(.8)
+}
+
+
+function round_number(value, places) {
+    if(places) {
+        var pow = Math.pow(10, places);
+        return Math.round(value * pow) / pow
+    }else {
+        return Math.round(value * 100) / 100
+    }
 }
